71e45b253d66f0f7c865348acbfe90fa948f66dc
bchaoss <103650247+bchaoss@users.noreply.github.com>
2026-02-24 10:39:23 +0000

    auto set local token (questionable)

---
+++ reports/scripts/inject-local-token.sh
@@ -0,0 +1,10 @@
+# #!/bin/bash
+
+# if [ -z "$MOTHERDUCK_TOKEN" ]; then
+#   echo "MOTHERDUCK_TOKEN not set"
+#   exit 1
+# fi
+
+# TOKEN_NAME="EVIDENCE_SOURCE__us_tariff_dataset__token"
+
+# sed -i "s|$TOKEN_NAME:.*|$TOKEN_NAME: $MOTHERDUCK_TOKEN|" .env
