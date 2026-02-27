6b91398edc1a15c89b3dc5fff2dc06bbd80d9867
bchaoss <103650247+bchaoss@users.noreply.github.com>
2026-02-27 22:43:49 +0800

    model dim_region_lookup

--- pipeline/models/marts/fact_us_trade_exports_country_monthly.sql
+++ pipeline/models/marts/fact_us_trade_exports_country_monthly.sql
@@ -1,20 +1,13 @@
-with region_code AS (
-  from {{ source('raw', 'us_trade_exports_country_monthly') }}
-  select cty_code, cty_name
-  where cty_code like '%XXX' -- Regions
-  group by all
-)
-
 from {{ source('raw', 'us_trade_exports_country_monthly') }} a
-left join region_code c
-  on SUBSTR(a.cty_code,1,1) = SUBSTR(c.cty_code,1,1)
+left join {{ ref('dim_region_lookup') }} c
+  on SUBSTR(a.cty_code,1,1) = c.region_code
 
 select
   DF,
 
   a.CTY_CODE,
   a.CTY_NAME,
-  c.CTY_NAME AS REGION_NAME,
+  c.REGION_NAME,
 
   YEAR(STRPTIME(time, '%Y-%m'))  as year,
   SUBSTR(time, 6, 2) as month,
